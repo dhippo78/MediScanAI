975eb6d47e016f76822f5f607f6d4e164a12fd18
Deji Ayeni <dhippo78@hotmail.com>
2026-05-17 13:19:39 +0100

    Credits formating

--- CreditsActivity.cs
+++ CreditsActivity.cs
@@ -19,16 +19,19 @@ namespace MediScanAI
             // Find the TextView and make the hyperlink active
             var creditsView = FindViewById<TextView>(Resource.Id.credits_text_Git_TextView);
             var creditsView2 = FindViewById<TextView>(Resource.Id.credits_text_TMAI_TextView);
+            var creditsView3 = FindViewById<TextView>(Resource.Id.credits_text_TFLite_TextView);
 
             // 2. Fetch the raw HTML string from resources
             string rawHtml = GetText(Resource.String.credits_text_Git);
             string rawHtml2 = GetText(Resource.String.credits_text_TMAI);
+            string rawHtml3 = GetText(Resource.String.credits_text_TFLite);
 
             // 3. Convert the HTML string into a stylized Spannable string
             if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.N)
             {
                 creditsView.TextFormatted = Android.Text.Html.FromHtml(rawHtml, Android.Text.FromHtmlOptions.ModeLegacy);
                 creditsView2.TextFormatted = Android.Text.Html.FromHtml(rawHtml2, Android.Text.FromHtmlOptions.ModeLegacy);
+                creditsView3.TextFormatted = Android.Text.Html.FromHtml(rawHtml3, Android.Text.FromHtmlOptions.ModeLegacy);
             }
             else
             {
@@ -36,12 +39,14 @@ namespace MediScanAI
                 #pragma warning disable CS0618
                 creditsView.TextFormatted = Android.Text.Html.FromHtml(rawHtml);
                 creditsView2.TextFormatted = Android.Text.Html.FromHtml(rawHtml2);
+                creditsView3.TextFormatted = Android.Text.Html.FromHtml(rawHtml3);
                 #pragma warning restore CS0618
             }
 
             // 4. Activate the link click listener logic
             creditsView.MovementMethod = Android.Text.Method.LinkMovementMethod.Instance;
             creditsView2.MovementMethod = Android.Text.Method.LinkMovementMethod.Instance;
+            creditsView3.MovementMethod = Android.Text.Method.LinkMovementMethod.Instance;
         }
     }
 }
